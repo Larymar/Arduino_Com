9fd83068e3c21ab814bd693201fa804a5fd867b1
Larymar <f98f96@gmail.com>
2017-05-27 00:40:48 +0300

    версия_1.0

--- Arduino_Com/MainWindow.xaml.cs
+++ Arduino_Com/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Text;
 using System.IO.Ports;
 using System.Windows;
@@ -14,12 +15,12 @@ namespace Arduino_Com
 
         private bool portstatus = false;
         private SerialPort myPort;
+
         public MainWindow()
         {
             InitializeComponent();
-            Start_button.Click += (sender, e) => myPort.DataReceived += MyPort_DataReceived;
+            Start_button.Click += (sender, e) => { if (portstatus) myPort.DataReceived += MyPort_DataReceived; };
             ALL_COM_SHOW();
-            CBinit();
         }
 
         private void Start_button_Click(object sender, RoutedEventArgs e)
@@ -29,9 +30,23 @@ namespace Arduino_Com
         private void init_Port()
         {
             int baud;
-            if (COMports.SelectedItem.ToString() != "COMPort" && !portstatus && int.TryParse(Baud.Text,out baud))
+            var baudstat = int.TryParse(Baud.Text, out baud);
+            if (!baudstat && !portstatus)
             {
-                myPort = new SerialPort(COMports.SelectedItem.ToString(),baud );
+                MessageBox.Show("Проверьте значение baud,\n и введите его", "Baud",MessageBoxButton.OK,MessageBoxImage.Warning);
+                Baud.Text = "baud";
+            }
+            if(!portstatus && COMports.SelectedItem.ToString() == "COMPort")
+            {
+                MessageBox.Show("Проверьте, выбран ли порт,\n и подключена ли плата", "Port", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            if (COMports.SelectedItem.ToString() != "COMPort" && !portstatus && baudstat)
+            {
+                int i = 5;
+                if (myPort != null)
+                    myPort.DataReceived -= this.MyPort_DataReceived;
+
+                myPort = new SerialPort(COMports.SelectedItem.ToString(), baud);
                 myPort.Open();
                 Start_button.Content = "Закрыть";
                 portstatus = true;
@@ -48,55 +63,55 @@ namespace Arduino_Com
 
         }
 
-      
+
 
         private void MyPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
         {
             var buff = new byte[myPort.BytesToRead];
-            myPort.Read(buff, 0, buff.Length);
-            var indata = Encoding.UTF8.GetString(buff);
-           // var indata = myPort.ReadLine();
+
+            var cb = myPort.Read(buff, 0, buff.Length);
+
+            // System.Diagnostics.Debug.Print(string.Join(" ", buff.Take(cb).Select(n => Convert.ToString(n, 16).PadLeft(2, '0'))));
+            var indata = Encoding.UTF8.GetString(buff, 0, cb);
+            // var indata = myPort.ReadLine();
 
             // indata used by other thread, how to solve this problem
-            //listBox.Dispatcher.Invoke(() => listBox.Items.Add(indata));
-            this.Dispatcher.Invoke(() => log.Text += indata);
-            Thread.Sleep(10);
+            //listBox.Dispatcher.Invoke(() => listBox.Items.Add(indata
+            //TODO: Поправить ошибку
+            Dispatcher.InvokeAsync(() => {
+                log.Text += indata;
+            }, System.Windows.Threading.DispatcherPriority.ApplicationIdle);
+            // Thread.Sleep(10);
         }
-    
-        private void CBinit()
-        {
-            File.Items.Add("File");
-            File.SelectedIndex = 0;
-        }
+
+     
 
         private void Send_button_Click(object sender, RoutedEventArgs e)
         {
-            myPort.Write(Input_box.Text);
+            string sendtext = Input_box.Text.ToString();
             
+            myPort.Write(sendtext);
+
         }
         private void ALL_COM_SHOW()
         {
             COMports.Items.Clear();
             string[] ports = SerialPort.GetPortNames();
-           
+
             COMports.Items.Add("COMPort");
             foreach (string line in ports)
             {
                 COMports.Items.Add(line);
             }
+
             COMports.SelectedIndex = 0;
             if (ports.Length > 1)
             {
-                COMports.SelectedIndex = 1;
+                COMports.SelectedIndex = 2;
             }
+
+            //System.Diagnostics.Process.Start()
         }
-
-        private void ButtonClose_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-       
 
         private void log_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
         {
@@ -112,6 +127,11 @@ namespace Arduino_Com
         private void Clean_Click(object sender, RoutedEventArgs e)
         {
             log.Clear();
+        }
+
+        private void COMports_MouseDown(object sender, EventArgs e)
+        {
+            ALL_COM_SHOW();
         }
     }
 }
